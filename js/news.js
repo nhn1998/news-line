7855f16880016cd82fd1db8926262937979f804a
nhn1998 <nhnayeem1@gmail.com>
2022-09-03 04:14:43 +0600

    news section

--- js/news.js
+++ js/news.js
@@ -11,10 +11,20 @@ const displayHeaderData = (values)=>{
     for(const value of values){
         const headDiv = document.createElement('div')
         headDiv.innerHTML= `
-            <a class="p-2 text-dark text-decoration-none">${value.category_name}</a>
+            <a onclick= "displayNews()" class="p-2 text-dark text-decoration-none">${value.category_name}</a>
         `;
         getCatagoriesData.appendChild(headDiv)
     }
 }
+const loadNews = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a')
+    .then(res => res.json())
+    .then(data =>displayNews(data.data[0]))
+    .catch(error =>console.log(error))
+}
+const displayNews =(news)=>{
+    console.log(news)
+}
+
 
 loadHeaderData()
