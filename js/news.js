3cb7799d073364842f66741fa4236466700a21e5
nhn1998 <nhnayeem1@gmail.com>
2022-09-04 00:09:22 +0600

    modal add

--- js/news.js
+++ js/news.js
@@ -58,11 +58,14 @@ const displayNews = newses=>{
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                      <h5 class="modal-title" id="staticBackdropLabel">Details about the news</h5>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                      ...
+                      <p>${news.author.name}</p>
+                      <p>${news.author.published_date}</p>
+                      <p>catagorie id : ${news.category_id}</p>
+                      <p>rating:${news.rating.badge} ${news.rating.number}</p>
                     </div>
                     <div class="modal-footer">
                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
